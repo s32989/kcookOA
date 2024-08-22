4d6629e09424c8f0a215d3a4fba830456306c1e6
s32989 <s32989@gmail.com>
2024-08-21 21:09:15 -0400

    adding gitignore

--- kcookOA_stock_ticker/stocks/models.py
+++ kcookOA_stock_ticker/stocks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+    symbol = models.CharField(max_length=10) # the max length of US stock tickers is 9?
+    user = models.CharField(max_length=100)
+
+class Alert(models.Model):
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    threshold = models.FloatField()
+    alert_type = models.CharField(max_length=10)
